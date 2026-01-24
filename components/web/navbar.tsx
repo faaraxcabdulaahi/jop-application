7ee8ad792a82bc1c1694cb020b70821c97861231
faaraxcabdulaahi <barashoonline2021@gmail.com>
2026-01-24 15:22:45 +0300

    finished the hero section and navbar task

--- components/web/navbar.tsx
+++ components/web/navbar.tsx
@@ -6,18 +6,24 @@ import { Button } from "../ui/button";
 const Navbar = () => {
   return (
     <nav className="border-b border-gray-200 bg-white">
-      <div className="container mx-auto flex h-16 items-center px-4">
+      <div className="container mx-auto flex h-16 items-center px-4 justify-between">
         <Link
           href="/"
           className="flex items-center space-x-2 gap-2 text-xl font-semibold text-primary"
         >
           <Briefcase />
         </Link>
-        <div>
+        <div className="flex items-center gap-2 text-xl font-semibold text-primary">
           <Link href="/signin">
-            <Button>Log In</Button>
+            <Button variant="ghost" className="text-gray-700 hover:text-black">
+              Log In
+            </Button>
           </Link>
-          <Link href="/signup">Start for free</Link>
+          <Link href="/signup">
+            <Button className="bg-primary hover:bg-primary/90">
+              Start for free
+            </Button>
+          </Link>
         </div>
       </div>
     </nav>
