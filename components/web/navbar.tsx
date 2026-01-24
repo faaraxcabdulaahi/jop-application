9fa72e30d7e971313c682f7e08fca32aff7766a3
faaraxcabdulaahi <barashoonline2021@gmail.com>
2026-01-24 14:41:48 +0300

    add navbar.tsx in the web folder inside the components

---
+++ components/web/navbar.tsx
@@ -0,0 +1,12 @@
+import { Briefcase } from "lucide-react";
+import React from "react";
+
+const Navbar = () => {
+  return <nav>
+    <div>
+      <Briefcase/>
+    </div>
+  </nav>;
+};
+
+export default Navbar;
