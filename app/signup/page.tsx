c2560ca0aec6803fe0a109ea2c8757342d1d870d
faaraxcabdulaahi <barashoonline2021@gmail.com>
2026-01-25 16:52:28 +0300

    finished the structure of the signup page

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,7 +1,51 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
 import React from "react";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <div className="flex min-h-[calc(100vh-4rem)] items-center justify-center bg-white p-4">
+      <Card>
+        <CardHeader>
+          <CardTitle>Sign Up</CardTitle>
+          <CardDescription>
+            Create an account to start tracking your jop application
+          </CardDescription>
+        </CardHeader>
+        <form>
+          <CardContent>
+            <div>
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" type="text" placeholder="John Doe" required />
+            </div>
+            <div>
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" type="email" placeholder="JohnDoe@example.com" required />
+            </div>
+            <div>
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" type="password" placeholder="********" required />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button type="submit">SignUp</Button>
+            <p>Already have an account ? <Link href="/signin">Sign In</Link></p>
+          </CardFooter>
+        </form>
+      </Card>
+    </div>
+  );
 };
 
 export default SignUp;
