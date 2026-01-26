bdaae8264ae822c682c9c30089dd24bc9d93eb0c
faaraxcabdulaahi <barashoonline2021@gmail.com>
2026-01-26 21:31:21 +0300

    added styles to the dropDownMenuContent

--- components/web/home/navbar.tsx
+++ components/web/home/navbar.tsx
@@ -45,11 +45,11 @@ const Navbar =  () => {
                     </Avatar>
                   </Button>
                 </DropdownMenuTrigger>
-                <DropdownMenuContent>
-                  <DropdownMenuLabel>
-                    <div>
-                      <p>{session.user.name}</p>
-                      <p>{session.user.email}</p>
+                <DropdownMenuContent className="w-56" align="end">
+                  <DropdownMenuLabel className="font-normal">
+                    <div className="flex flex-col space-y-1">
+                      <p className="text-sm font-medium leading-none">{session.user.name}</p>
+                      <p className="text-xs leading-none text-muted-foreground">{session.user.email}</p>
                     </div>
                   </DropdownMenuLabel>
                   <SignOutButton />
--- components/web/home/signOutButton.tsx
+++ components/web/home/signOutButton.tsx
@@ -1,10 +1,22 @@
-"use client"
+"use client";
 import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
 import { signOut } from "@/lib/auth/authClient";
+import { router } from "better-auth/api";
+import { useRouter } from "next/navigation";
 
 const SignOutButton = () => {
+    const router = useRouter();
   return (
-    <DropdownMenuItem onClick={async () => await signOut()}>
+    <DropdownMenuItem
+      onClick={async () => {
+        const result = await signOut();
+        if (result.data) {
+            router.push("/sign-in")
+        }else{
+            alert("Error signing out")
+        }
+      }}
+    >
       Logout
     </DropdownMenuItem>
   );
