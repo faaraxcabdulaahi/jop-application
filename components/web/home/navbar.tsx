88e5adedb17302d760567d2ad57a07ceff014d74
faaraxcabdulaahi <barashoonline2021@gmail.com>
2026-01-26 19:50:35 +0300

    apply session logic which make it disappear login and signup and it shows dashboard button which directs to the dashboard page

--- components/web/home/navbar.tsx
+++ components/web/home/navbar.tsx
@@ -2,8 +2,10 @@ import { Briefcase } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 import { Button } from "../../ui/button";
+import { getSession } from "@/lib/auth/auth";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await getSession();
   return (
     <nav className="border-b border-gray-200 bg-white">
       <div className="container mx-auto flex h-16 items-center px-4 justify-between">
@@ -14,16 +16,34 @@ const Navbar = () => {
           <Briefcase />
         </Link>
         <div className="flex items-center gap-2 text-xl font-semibold text-primary">
-          <Link href="/sign-in">
-            <Button variant="ghost" className="text-gray-700 hover:text-black">
-              Log In
-            </Button>
-          </Link>
-          <Link href="/sign-up">
-            <Button className="bg-primary hover:bg-primary/90">
-              Start for free
-            </Button>
-          </Link>
+          {session?.user ? (
+            <>
+              <Link href="/dashboard">
+                <Button
+                  variant="ghost"
+                  className="text-gray-700 hover:text-black"
+                >
+                  Dashboard
+                </Button>
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link href="/sign-in">
+                <Button
+                  variant="ghost"
+                  className="text-gray-700 hover:text-black"
+                >
+                  Log In
+                </Button>
+              </Link>
+              <Link href="/sign-up">
+                <Button className="bg-primary hover:bg-primary/90">
+                  Start for free
+                </Button>
+              </Link>
+            </>
+          )}
         </div>
       </div>
     </nav>
