db0f977256c367dd2bdee14b5569dc08d16d154a
faaraxcabdulaahi <barashoonline2021@gmail.com>
2026-02-07 19:11:52 +0300

    add index.ts file which exports all of our models

---
+++ lib/models/index.ts
@@ -0,0 +1,7 @@
+import "./board";
+import "./column";
+import "./jopApplication";
+
+export {default as Board} from "./board";
+export {default as Column} from "./column";
+export {default as jopApplication} from "./jopApplication";
