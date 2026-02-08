62d4f3cf33ce012208aec09ebb9fea704c40cbf0
faaraxcabdulaahi <barashoonline2021@gmail.com>
2026-02-08 18:17:15 +0300

    add logic which is checking if the user is in signIn or signUp page and then returning him back to the dashboard

--- proxy.ts
+++ proxy.ts
@@ -8,10 +8,17 @@ export const proxy = async (request: NextRequest) => {
   // Getting the "session" iam in
   const session = await getSession();
   // Getting the "page" iam in
-  const isDashboardPage = request.nextUrl.pathname.startsWith("/dashboard")
+  const isDashboardPage = request.nextUrl.pathname.startsWith("/dashboard");
   if (isDashboardPage && !session?.user) {
     return NextResponse.redirect(new URL("/sign-in", request.url));
-  };
+  }
+
+  const isSignInPage = request.nextUrl.pathname.startsWith("/dashboard");
+    const isSignUpPage = request.nextUrl.pathname.startsWith("/dashboard")
+
+  if ((isSignInPage || isSignUpPage) && session?.user) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
 
   return NextResponse.next();
 };
