4f054d7892f2369bc5e9bbf8305c0a2628b3032c
faaraxcabdulaahi <barashoonline2021@gmail.com>
2026-02-08 19:32:57 +0300

    added the Board type interface

--- lib/types/index.ts
+++ lib/types/index.ts
@@ -1,21 +1,28 @@
 interface JopApplication {
-    _id:string;
-    company:string;
-    position:string;
-    location?:string;
-    status:string;
-    notes?:string;
-    salary?:string;
-    jopUrl?:string;
-    order:number;
-    columnId?:string;
-    tags?:string[];
-    description?:string;
-};
+  _id: string;
+  company: string;
+  position: string;
+  location?: string;
+  status: string;
+  notes?: string;
+  salary?: string;
+  jopUrl?: string;
+  order: number;
+  columnId?: string;
+  tags?: string[];
+  description?: string;
+}
 
 interface Column {
+  _id: string;
+  name: string;
+  order: number;
+  jopApplications: JopApplication[];
+}
+
+
+interface Board {
     _id:string;
     name:string;
-    order:number;
-    jopApplications:JopApplication[];
+    columns:Column[];
 }
